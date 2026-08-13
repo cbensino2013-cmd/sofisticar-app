571141118a6d2f18bc79b571e451f05c12b1aff9
cbensino2013-cmd <cb.ensino2013@gmail.com>
2026-08-13 21:16:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -311,4 +311,9 @@ def pagina_painel(pin: str = ""):
     </body>
     </html>
     """
+    if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
     return html
